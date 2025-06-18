dd011ada663689951c10f3a24d73413187ab9976
LUIZ EDUARDO SELLMER VIEIRA <luiz.vieira2@alunos.sc.senac.br>
2025-06-18 13:46:12 -0300

    Atualizado layout da Home

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,25 @@
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 import { Link } from 'expo-router';
-import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome, Feather, MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.titulo}>Ecological Construction</Text>
+
+      <Image source={require('@/assets/images/vaso-retangular.jpg')} style={styles.imagemHorizontal} />
+
+      <View style={styles.imagemLinha}>
+        <Image source={require('@/assets/images/vaso-quadrado.jpg')} style={styles.imagemQuadrada} />
+        <Image source={require('@/assets/images/vaso-quadrado2.jpg')} style={styles.imagemQuadrada} />
+      </View>
+
+      <Image source={require('@/assets/images/tijolo-ilus-retangulo.jpg')} style={styles.imagemHorizontal} />
+
+      <View style={styles.imagemLinha}>
+        <Image source={require('@/assets/images/tijolo-ilus-quadrado.webp')} style={styles.imagemQuadrada} />
+        <Image source={require('@/assets/images/tijolo-ilus-quadrado2.jpg')} style={styles.imagemQuadrada} />
+      </View>
 
     </View>
   );
@@ -21,8 +35,31 @@ const styles = StyleSheet.create({
     fontSize: 28,
     marginTop: 50,
     marginBottom: 20,
-    borderRadius: 15,
-    padding: 20,
+    fontWeight: 'bold',
+    fontFamily: 'serif',
+    color: '#2E1F0F',
+    top: 10,
+  },
+  imagemHorizontal: {
+    width: '90%',
+    height: 120,
+    resizeMode: 'cover',
+    marginBottom: 10,
+    borderRadius: 10,
+    top: 30,
+  },
+  imagemLinha: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '90%',
+    marginBottom: 80,
+  },
+  imagemQuadrada: {
+    width: '45%',
+    height: 150,
+    resizeMode: 'cover',
+    borderRadius: 10,
+    top: 65,
   },
   menu: {
     flexDirection: 'row',
